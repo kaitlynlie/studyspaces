e59c2df7c0cc49b817955111f1085f09d9c79b06
kaitlyn li <kaitlynli0903@gmail.com>
2023-09-29 23:44:52 -0700

    first commit: need to change slider color and other tabs

---
+++ src/pages/Navbar.tsx
@@ -0,0 +1,74 @@
+import React from 'react'
+import styles from './Navbar.module.scss'
+import clsx from 'clsx'
+import { title } from '../assets'
+import { useState } from 'react'
+import { RiMenu3Line, RiCloseLine } from 'react-icons/ri';
+import { Dialog, DialogTitle, DialogContent, DialogActions, Button } from '@material-ui/core';
+
+const Navbar = () => {
+  const [toggleMenu, setToggleMenu] = useState(false);
+  const [openTodo, setOpenTodo] = useState(false);
+  const [openTimer, setOpenTimer] = useState(false);
+  
+  const handleClickOpenTodo = () => {
+    setOpenTodo(true);
+  };
+  
+  const handleClickOpenTimer = () => {
+    setOpenTimer(true);
+  };
+  
+  const handleClose = () => {
+    setOpenTodo(false);
+    setOpenTimer(false);
+  };
+
+  return (
+    <nav className={clsx(styles.navbar)}>
+    <div className={clsx(styles.toggle)}>
+        {toggleMenu
+          ? <RiCloseLine color="#000" size={27} onClick={() => setToggleMenu(false)} />
+          : <RiMenu3Line color="#000" size={27} onClick={() => setToggleMenu(true)} />}
+        {toggleMenu && (
+        <div className={clsx(styles.box)}>
+          <div className="none">
+            <ul className={clsx(styles.dropdown)}>
+              <li>
+                <a onClick={handleClickOpenTodo}>Todo</a>
+              </li>
+              <li>
+                <a onClick={handleClickOpenTimer}>Timer</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+        )}
+        </div>
+
+    {/* Todo Dialog */}
+    <Dialog open={openTodo} onClose={handleClose}>
+      <DialogActions>
+      <RiCloseLine color="#000" size={27} onClick={handleClose} />
+      </DialogActions>
+      <DialogTitle>Todo</DialogTitle>
+      <DialogContent>
+        {/* Add your content for the Todo dialog here */}
+      </DialogContent>
+    </Dialog>
+
+    {/* Timer Dialog */}
+    <Dialog open={openTimer} onClose={handleClose} >
+      <DialogActions>
+      <RiCloseLine color="#000" size={27} onClick={handleClose} />
+      </DialogActions>
+      <DialogTitle>Timer</DialogTitle>
+      <DialogContent>
+        {/* Add your content for the Timer dialog here */}
+      </DialogContent>
+    </Dialog>
+  </nav>
+  )
+}
+
+export default Navbar
